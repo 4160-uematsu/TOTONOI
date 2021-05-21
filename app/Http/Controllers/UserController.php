60c23b18dbab3ff67433c6d2480341f00f15cd9c
4160-uematsu <radyouichi@gmail.com>
2021-05-21 20:51:29 +0900

    コメント名を表示させました

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,23 +17,30 @@ class UserController extends Controller
 
     public function update(Request $request)
     {   
-        // 画像のアップロード
-        $path = $request->file('image')->store('public/avatar');
-        $image_path = basename($path);
+        // 投稿内容の受け取って変数に入れる
+        $name = $request->input('name');
+        $comment = $request->input('comment');
         
-        // // DBにアップロードする
-        $image_path2 = new Image();
-        $image_path2->fill([
-            'user_id'=>1,
-            'image_name'=>'',	
-            'image_path'=>$image_path,
-            ]);
-            $array_2=(array)$image_path2;
-            $image_path2->fill($array_2)->save();    
-
-
-        return redirect("/users/edit")->with("message", "アップロードに成功しました。");
-        // $image_path2->image_pass = $image_path;
-        // $image_path2->save();
+        if($request->hasFile('image')) {
+            // 画像のアップロード
+            $path = $request->file('image')->store('public/avatar');
+            $image_path = basename($path);
+        
+            // // DBにアップロードする
+            $image_path2 = new Image();
+            $image_path2->fill([
+                'user_id'=>1,
+                'image_name'=>'',	
+                'image_path'=>$image_path,
+                ]);
+                $array_2=(array)$image_path2;
+                $image_path2->fill($array_2)->save();
+        }       
+        
+        
+        return redirect("/users/edit")->with([
+                "message" => "投稿に成功しました。",
+                "name" => $name,
+                "comment"  => $comment]);
     }
 }
--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,14 +1,31 @@
+<html>
+<body>
 @if (Session::has('message'))
     <p>{{ session('message') }}</p>
 @endif
 
+@if(Session::has('name'))
+<h2>{{ session('name') }}さんの直前の投稿</h2>
+{{ session('comment') }}
+<br><hr>
+@endif
+
+
+<!-- 投稿画面 -->
 <form action="/users/edit" method="post" enctype='multipart/form-data'> 
-        {{ csrf_field() }}
-        <div>
-            <input type="hidden" name="id" value="">
-        </div>
-        <div>
-            <input type="file" name="image">
-        </div>
-        <input type="submit" value="画像を送信する">
+    {{ csrf_field() }}
+    <p>口コミ投稿フォーム</p>
+    <input type="text" name="name"><br><br>
+    <textarea name="comment" rows="8" cols="40">
+    </textarea><br><br>
+    <!-- 画像アップロード -->
+    <div>
+        <input type="hidden" name="id" value="">
+    </div>
+    <div>
+        <input type="file" name="image">
+    </div>
+    <input type="submit" value="投稿する">
 </form>
+</body>
+</html>
